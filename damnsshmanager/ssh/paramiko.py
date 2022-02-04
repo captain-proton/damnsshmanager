66584cb8559bdad821aff4b504776edb185eb3f5
Nils <nverheyen@googlemail.com>
2022-02-04 06:09:32 +0100

    Exchange flake8 with pylint

--- damnsshmanager/ssh/paramiko.py
+++ damnsshmanager/ssh/paramiko.py
@@ -1,3 +1,5 @@
+"""This module contains classes to open a ssh connection
+with the paramiko library"""
 import os
 import socket
 import sys
@@ -18,52 +20,57 @@ _msg = Config.messages
 
 
 class ParamikoConnector(SSHConnector):
+    """This connector"""
 
     def connect(self, host: Host, ltun: Optional[LocalTunnel]) -> None:
         with paramiko.SSHClient() as client:
 
+            known_hosts_path = "~/.ssh/known_hosts"
             try:
-                known_hosts_path = "~/.ssh/known_hosts"
                 client.load_host_keys(os.path.expanduser(known_hosts_path))
             except IOError:
-                logger.error(_msg.get("err.msg.io.known_hosts", known_hosts_path))
+                logger.error(
+                    _msg.get("err.msg.io.known_hosts", known_hosts_path))
 
             client.load_system_host_keys()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             try:
-                client.connect(host.addr, port=host.port, username=host.username)
+                client.connect(host.addr, port=host.port,
+                               username=host.username)
 
                 tun = None
                 if ltun is not None and isinstance(ltun, LocalTunnel):
                     tun = sshtunnel.open_tunnel((host.addr, host.port),
                                                 ssh_username=host.username,
-                                                remote_bind_address=(ltun.destination, ltun.rport),
+                                                remote_bind_address=(
+                                                    ltun.destination, ltun.rport),
                                                 local_bind_address=('', ltun.lport))
                 logger.info(_msg.get("new.interactive.shell"))
                 channel = client.invoke_shell()
                 self.open_interactive_shell(channel)
                 if tun:
                     tun.close()
-            except paramiko.ssh_exception.BadHostKeyException:
-                logger.error(_msg.get("err.msg.invalid.server.host.key", known_hosts_path))
-            except paramiko.ssh_exception.AuthenticationException:
+            except paramiko.BadHostKeyException:
+                logger.error(
+                    _msg.get("err.msg.invalid.server.host.key", known_hosts_path))
+            except paramiko.AuthenticationException:
                 logger.error(_msg.get("err.msg.ssh.auth", host.addr))
             except socket.error:
                 logger.error(_msg.get("err.msg.socket"))
 
     def open_interactive_shell(self, channel: paramiko.Channel):
         if sys.platform.startswith('linux'):
-            channel = PosixChannel(channel)
+            chan = PosixChannel(channel)
         else:
-            channel = DefaultChannel(channel)
+            chan = DefaultChannel(channel)
 
-        channel.open()
+        chan.open()
 
 
 @dataclass
 class PosixChannel:
 
-    chan: socket.socket
+    ssh_channel: paramiko.Channel
 
     def open(self):
         import select
@@ -74,34 +81,40 @@ class PosixChannel:
             tty.setraw(sys.stdin.fileno())
             tty.setcbreak(sys.stdin.fileno())
 
-            self.chan.settimeout(0.0)
+            self.ssh_channel.settimeout(0.0)
 
             stop = False
             while not stop:
-                r, w, e = select.select([self.chan, sys.stdin], [], [])
-                if self.chan in r:
+                r_list, _, _ = select.select(
+                    [self.ssh_channel, sys.stdin], [], [])
+                if self.ssh_channel in r_list:
                     try:
-                        x = to_unicode(self.chan.recv(1024))
-                        if not x:
-                            x = "\r\n*** Bye bye\r\n"
+                        input_char = to_unicode(self.ssh_channel.recv(1024))
+                        if not input_char:
+                            input_char = "\r\n*** Bye bye\r\n"
                             stop = True
-                        sys.stdout.write(x)
+                        sys.stdout.write(input_char)
                         sys.stdout.flush()
                     except socket.timeout:
                         logger.error(_msg.get("err.msg.socket.timeout"))
 
-                if sys.stdin in r:
-                    x = sys.stdin.read(1)
-                    if not x:
+                if sys.stdin in r_list:
+                    input_char = sys.stdin.read(1)
+                    logger.debug(f'read {input_char!r} from stdin,'
+                                 ' type of char {type(input_char)!r}')
+                    if not input_char:
                         stop = True
                     else:
-                        self.chan.send(x)
+                        self.ssh_channel.send(input_char.encode())
         finally:
             sys_tty = termios.tcgetattr(sys.stdin)
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, sys_tty)
 
 
+@dataclass
 class DefaultChannel:
+
+    chan: paramiko.Channel
 
     def open(self):
         pass
