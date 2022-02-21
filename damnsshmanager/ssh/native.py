b17c1700e3caa5d1ee7f3f311f4b18e602824db6
Nils Verheyen <nils.verheyen@uni-due.de>
2022-02-21 11:50:48 +0100

    Removed connection setup logic to submodules

--- damnsshmanager/ssh/native.py
+++ damnsshmanager/ssh/native.py
@@ -1,16 +1,37 @@
 import subprocess
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
+
+from loguru import logger
 
 from ..hosts import Host
 from ..localtunnel import LocalTunnel
-from .ssh_connector import SSHConnector
+from .channel import SSHChannel
 
 
 @dataclass
-class NativeConnector(SSHConnector):
+class NativeChannel(SSHChannel):
 
-    def connect(self, host: Host, ltun: Optional[LocalTunnel]) -> None:
+    _host: Host = field(default=None, init=False)
+    _completed_process: subprocess.CompletedProcess = field(
+        default=None, init=False)
+    _proc_error: subprocess.CalledProcessError = field(
+        default=None, init=False)
+
+    def __enter__(self):
+        ...
+
+    def __exit__(self, exc_type, exc_value, trace):
+        if self._completed_process:
+            logger.info(f'Connection to {self._host} closed')
+
+        if self._proc_error:
+            logger.error(
+                f'An error communicating with {self._host}'
+                f': {self._proc_error}')
+
+    def open(self, host: Host, ltun: Optional[LocalTunnel] = None) -> None:
+        self._host = host
         cmd = 'ssh -p {port:d}'
         cmd = cmd.format(port=host.port)
 
@@ -21,4 +42,9 @@ class NativeConnector(SSHConnector):
 
         cmd = ' '.join([cmd, '{user}@{hostname}'])
         cmd = cmd.format(user=host.username, hostname=host.addr)
-        subprocess.call(cmd, shell=True)
+        try:
+            self._completed_process = subprocess.run(cmd,
+                                                     shell=True,
+                                                     check=True)
+        except subprocess.CalledProcessError as err:
+            self._proc_error = err
